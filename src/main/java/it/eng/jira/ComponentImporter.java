19a9b5459c03540e7082c684ebef2cce9a558d16
luandrea <luca.andreatta@eng.it>
2018-07-13 14:38:52 +0200

    - put correct class in ComponentImport

--- src/main/java/it/eng/jira/ComponentImporter.java
+++ src/main/java/it/eng/jira/ComponentImporter.java
@@ -1,21 +1,24 @@
 package it.eng.jira;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-import com.atlassian.jira.security.PermissionManager;
+import com.atlassian.jira.security.GlobalPermissionManager;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 
 /**
  * This class exists solely to cause Spring Scanner to import
  * the required OSGi services. It should never be instantiated.
  */
+@Component("myComponentImporter")
 public class ComponentImporter {
 
-	private PermissionManager permissionMgr;
+	private GlobalPermissionManager GlobalPermissionManager;
+
 
 	@Autowired
-	public ComponentImporter (@ComponentImport PermissionManager permissionManager) {
-		this.permissionMgr = permissionManager;
+	public ComponentImporter (@ComponentImport GlobalPermissionManager GlobalPermissionManager) {
+		this.GlobalPermissionManager = GlobalPermissionManager;
 		// no need to actually do anything with it...
 	}
 }
